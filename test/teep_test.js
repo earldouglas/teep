3904d7796288a279aead9375ecfad624ed10fad6
James Earl Douglas <james@earldouglas.com>
2014-11-16 15:35:30 -0800

    Fix option(false), which should not be empty

--- test/teep_test.js
+++ test/teep_test.js
@@ -29,17 +29,27 @@ exports['option'] = {
     // setup here
     done();
   },
-  'constructor: no args': function(test) {
+  'option()': function(test) {
     test.expect(2);
     // tests here
     test.equal(teep.option().empty, true, 'should be empty.');
     test.equal(teep.option().toString(), 'none()', 'should be none().');
     test.done();
   },
-  'constructor: one arg': function(test) {
+  'option(42)': function(test) {
     test.expect(2);
     test.equal(teep.option(42).empty, false, 'should not be empty.');
     test.equal(teep.option(42).toString(), 'some(42)', 'should be some(42).');
+    test.done();
+  },
+  'option(false)': function(test) {
+    test.expect(1);
+    test.equal(teep.option(false).toString(), 'some(false)', 'should not be empty.');
+    test.done();
+  },
+  'option(null)': function(test) {
+    test.expect(1);
+    test.equal(teep.option(null).toString(), 'none()', 'should be empty.');
     test.done();
   },
   'map': function(test) {
