9683be5080339b72c3db13686b602a0af680d952
James Earl Douglas <james@earldouglas.com>
2014-12-29 22:17:53 -0800

    Add missing tests for great coverage

--- test/validation_test.js
+++ test/validation_test.js
@@ -11,4 +11,17 @@ exports['validation'] = {
     test.equal(teep.invalid(['wat']).map(inc).toString(), 'invalid(wat)', 'should be invalid(wat).');
     test.done();
   },
+  'flatMap': function(test) {
+    function incLift(x) { return teep.valid(1 + x); }
+    test.expect(2);
+    test.equal(teep.valid(41).flatMap(incLift).toString(), 'valid(42)', 'should be 42.');
+    test.equal(teep.invalid(['wat']).flatMap(incLift).toString(), 'invalid(wat)', 'should be invalid(wat).');
+    test.done();
+  },
+  'ap': function(test) {
+    test.expect(2);
+    test.equal(teep.valid(inc).ap(teep.valid(41)).toString(), 'valid(42)', 'should be 42.');
+    test.equal(teep.invalid(['wat']).ap(teep.valid(41)).toString(), 'invalid(wat)', 'should be invalid(wat).');
+    test.done();
+  },
 };
